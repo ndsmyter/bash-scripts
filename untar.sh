9ed0872d20e0adac3c53a3d70eed5bcbdd8d312f
Nicolas De Smyter <nicolasdesmyter@gmail.com>
2018-12-19 11:55:33 +0100

    Using a different layout for the header

--- untar.sh
+++ untar.sh
@@ -1,5 +1,22 @@
 #! /bin/bash
 
-# Most of the time I cannot remember the parameters to untar, so I created an utility script to do this
+#===============================================================================
+#
+#          FILE:  untar.sh
+#
+#         USAGE:  ./untar.sh <tar-file>
+#
+#   DESCRIPTION:  Most of the time I cannot remember the parameters to untar, so I created an utility script to do this
+#
+#       OPTIONS:  <tar-file>: The path or name of the tar file that you want to untar
+#  REQUIREMENTS:  ---
+#          BUGS:  ---
+#         NOTES:  ---
+#        AUTHOR:  Nicolas De Smyter, nicolasdesmyter+github@gmail.com
+#       COMPANY:  ---
+#       VERSION:  1.0
+#       CREATED:  2018-12-19
+#      REVISION:  ---
+#===============================================================================
 
 tar xvzf $1
