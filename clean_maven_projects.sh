4732063dc4399189cf74f2e15e8b5d13522d85d1
Nicolas De Smyter <nicolasdesmyter@gmail.com>
2018-12-19 11:46:20 +0100

    Create clean_maven_projects.sh
    to cleanup Maven projects on disk

---
+++ clean_maven_projects.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+
+# This script will find all the "pom.xml" files on the disk.
+# The paths to these files are listed in the ${tmp_file} file.
+# After all "pom.xml" files are found, the script will start looping through them one by one
+# For every file, it will execute the command maven clean command to remove data that isn't used anymore
+# In the end it will cleanup everything and go back to the folder where the command started
+
+tmp_file=/tmp/files_to_cleanup.txt
+cwd=${pwd}
+
+echo "Finding all the pom.xml files on the disk..."
+sudo find / -name pom.xml -print > ${tmp_file} 2> /dev/null
+
+# Count the files that were found
+files=$(cat "${tmp_file}" | wc -l)
+echo "${files} files found"
+
+# Loop over files
+while read p; do
+    # Remove the "pom.xml" part at the end of the path, to get the parent director
+    folder=${p:0:-7}
+    echo "${folder}"
+    cd "${folder}"
+    # Execute "mvn -e clean" and make sure every output is sent to /dev/null (so all output is ignore)
+    mvn -e clean > /dev/null 2>&1
+done < ${tmp_file}
+
+# Cleanup and back to original folder
+cd ${cwd}
+rm ${tmp_file}
